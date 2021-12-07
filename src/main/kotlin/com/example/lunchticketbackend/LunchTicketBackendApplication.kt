7dd93d7368cf0071e5a8f4fdbd498e2a5422260c
Marco Pérez <marcopza99@hotmail.com>
2021-12-06 20:16:28 -0500

    Initial commit

---
+++ src/main/kotlin/com/example/lunchticketbackend/LunchTicketBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.lunchticketbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LunchTicketBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<LunchTicketBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/lunchticketbackend/LunchTicketBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.lunchticketbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LunchTicketBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
